c4875dd1153c7159978872cf447490d99d9635df
tommychang97 <tommychang97@gmail.com>
2022-07-19 20:47:41 -0700

    profile card with picture change and qr code gen

--- app/src/main/java/com/example/cardlink/fragments/UserQrFragment.kt
+++ app/src/main/java/com/example/cardlink/fragments/UserQrFragment.kt
@@ -1,18 +1,27 @@
 package com.example.cardlink.fragments
 
+import android.graphics.Bitmap
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
 import com.example.cardlink.R
+import net.glxn.qrgen.android.QRCode
 
 class UserQrFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_user_qr, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val myBitmap: Bitmap = QRCode.from("www.example.org").bitmap()
+        val myImage: ImageView = view.findViewById(R.id.qrImg) as ImageView
+        myImage.setImageBitmap(myBitmap)
     }
 }
