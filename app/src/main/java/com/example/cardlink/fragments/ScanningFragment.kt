9f9a6fc1d0355eef11de254199bac07d8919c3f4
Parshanl <>
2022-07-27 17:16:54 -0700

    added scanner camera implementation and external library

--- app/src/main/java/com/example/cardlink/fragments/ScanningFragment.kt
+++ app/src/main/java/com/example/cardlink/fragments/ScanningFragment.kt
@@ -1,13 +1,28 @@
 package com.example.cardlink.fragments
 
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.budiyev.android.codescanner.AutoFocusMode
+import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.CodeScannerView
+import com.budiyev.android.codescanner.DecodeCallback
+import com.budiyev.android.codescanner.ScanMode
 import com.example.cardlink.R
 
+private const val CAMERA_REQUEST_CODE = 101
+
 class ScanningFragment : Fragment() {
+
+    private lateinit var mQrScanner:CodeScanner
+    private lateinit var mContext: Context
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,4 +31,44 @@ class ScanningFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_scanning, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mContext = view.context
+        qrScannerSetUp(view)
+
+    }
+
+    private fun qrScannerSetUp(view: View){
+        val qrScanner = view.findViewById<CodeScannerView>(R.id.qrScanner)
+        mQrScanner = CodeScanner(mContext, qrScanner)
+
+        mQrScanner.apply {
+            camera = CodeScanner.CAMERA_BACK
+            formats = CodeScanner.ALL_FORMATS
+
+            autoFocusMode = AutoFocusMode.SAFE
+            scanMode = ScanMode.CONTINUOUS
+            isAutoFocusEnabled = true
+            isFlashEnabled = false
+
+//            decodeCallback = DecodeCallback {}
+
+            qrScanner.setOnClickListener {
+                mQrScanner.startPreview()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mQrScanner.startPreview()
+    }
+
+    override fun onPause() {
+        mQrScanner.releaseResources()
+        super.onPause()
+    }
+
+
 }
