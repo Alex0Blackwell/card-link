61a9ae7dbce159d8226e7393e43103f9c83b71a8
alex blackwell <alex.blackwell314@gmail.com>
2022-08-02 18:49:31 -0700

    Display primary key in QR code

--- app/src/main/java/com/example/cardlink/fragments/UserQrFragment.kt
+++ app/src/main/java/com/example/cardlink/fragments/UserQrFragment.kt
@@ -7,8 +7,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.LinearLayout
 import com.example.cardlink.R
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import net.glxn.qrgen.android.QRCode
 
 class UserQrFragment : Fragment() {
@@ -20,8 +21,14 @@ class UserQrFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val myBitmap: Bitmap = QRCode.from("www.example.org").bitmap()
-        val myImage: ImageView = view.findViewById(R.id.qrImg) as ImageView
-        myImage.setImageBitmap(myBitmap)
+
+        val user = Firebase.auth.currentUser
+        val userId = user?.uid
+        if(userId != null) {
+            println("debug: displaying user ID ($userId) on QR-code")
+            val myBitmap: Bitmap = QRCode.from(userId).bitmap()
+            val myImage: ImageView = view.findViewById(R.id.qrImg) as ImageView
+            myImage.setImageBitmap(myBitmap)
+        }
     }
 }
