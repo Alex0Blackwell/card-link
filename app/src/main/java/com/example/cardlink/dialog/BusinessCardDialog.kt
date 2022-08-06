ee842792ef35b727a2ee9b63d26c425004e2474b
kkuninaka <katelynnkuninaka@hotmail.com>
2022-08-06 14:25:06 -0700

    Make links icons clickable

--- app/src/main/java/com/example/cardlink/dialog/BusinessCardDialog.kt
+++ app/src/main/java/com/example/cardlink/dialog/BusinessCardDialog.kt
@@ -2,19 +2,23 @@ package com.example.cardlink.dialog
 
 import android.app.Dialog
 import android.content.DialogInterface
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
+import androidx.core.view.isVisible
 import androidx.fragment.app.DialogFragment
 import com.example.cardlink.R
 import com.example.cardlink.Util.Companion.downloadUserImage
 import com.example.cardlink.dataLayer.Mock
 import com.example.cardlink.dataLayer.MockContact
-import com.example.cardlink.dataLayer.Person
+
 
 class BusinessCardDialog: DialogFragment(), DialogInterface.OnClickListener {
 
@@ -65,6 +69,52 @@ class BusinessCardDialog: DialogFragment(), DialogInterface.OnClickListener {
                     downloadUserImage(uid, it)
                 }
             }
+
+        if (linkedIn != "") {
+            dialog.findViewById<ImageButton>(R.id.network_card_linkedin)?.isVisible = true
+            dialog.findViewById<ImageButton>(R.id.network_card_linkedin)?.isEnabled = true
+            dialog.findViewById<ImageButton>(R.id.network_card_linkedin)?.setOnClickListener { _ ->
+                val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(linkedIn))
+                startActivity(browserIntent)
+            }
+        }
+
+        if (github != "") {
+            dialog.findViewById<ImageButton>(R.id.network_card_github)?.isVisible = true
+            dialog.findViewById<ImageButton>(R.id.network_card_github)?.isEnabled = true
+            dialog.findViewById<ImageButton>(R.id.network_card_github)?.setOnClickListener { _ ->
+                val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(github))
+                startActivity(browserIntent)
+            }
+        }
+
+        if (facebook != "") {
+            dialog.findViewById<ImageButton>(R.id.network_card_facebook)?.isVisible = true
+            dialog.findViewById<ImageButton>(R.id.network_card_facebook)?.isEnabled = true
+            dialog.findViewById<ImageButton>(R.id.network_card_facebook)?.setOnClickListener { _ ->
+                val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(facebook))
+                startActivity(browserIntent)
+            }
+        }
+
+        if (twitter != "") {
+            dialog.findViewById<ImageButton>(R.id.network_card_twitter)?.isVisible = true
+            dialog.findViewById<ImageButton>(R.id.network_card_twitter)?.isEnabled = true
+            dialog.findViewById<ImageButton>(R.id.network_card_twitter)?.setOnClickListener { _ ->
+                val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(twitter))
+                startActivity(browserIntent)
+            }
+        }
+
+        if (website != "") {
+            dialog.findViewById<ImageButton>(R.id.network_card_website)?.isVisible = true
+            dialog.findViewById<ImageButton>(R.id.network_card_website)?.isEnabled = true
+            dialog.findViewById<ImageButton>(R.id.network_card_website)?.setOnClickListener { _ ->
+                val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(website))
+                startActivity(browserIntent)
+            }
+        }
+
         return dialog
     }
 
