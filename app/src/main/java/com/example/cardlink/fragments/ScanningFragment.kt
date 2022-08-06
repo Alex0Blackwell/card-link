bb18b573b6f470b34cb634115c8a6083134dc75d
alex blackwell <alex.blackwell314@gmail.com>
2022-08-06 14:42:29 -0700

    Add success/failure status on scan

--- app/src/main/java/com/example/cardlink/fragments/ScanningFragment.kt
+++ app/src/main/java/com/example/cardlink/fragments/ScanningFragment.kt
@@ -2,14 +2,14 @@ package com.example.cardlink.fragments
 
 import android.Manifest
 import android.content.Context
-import android.content.DialogInterface
 import android.content.pm.PackageManager
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -17,8 +17,11 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.budiyev.android.codescanner.*
 import com.example.cardlink.R
+import com.example.cardlink.Util
+import com.example.cardlink.dataLayer.Person
 import com.example.cardlink.viewModels.MainViewModel
-import com.google.zxing.Result
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 
 private const val CAMERA_REQUEST_CODE = 101
@@ -77,29 +80,74 @@ class ScanningFragment : Fragment() {
 
             profileViewModel.addConnection(it.toString())
 
-            requireActivity().runOnUiThread {
-                val builder: AlertDialog.Builder? = activity?.let {
-                    AlertDialog.Builder(it)
-                }
-                builder?.apply {
-                    setMessage("Data: $it")
-                    setTitle("Business Card")
-                    setNegativeButton("Cancel"
-                    ) { dialog, id ->
+            val database = Firebase.database.reference
+            try {
+                val receiverUserRef = database.child("users").child(it.toString())
+                receiverUserRef.get().addOnSuccessListener { receiverUser ->
 
-
+                    requireActivity().runOnUiThread {
+                        val contact = Person(
+                            it.toString(),
+                            Util.asString(receiverUser.child("name").value),
+                            Util.asString(receiverUser.child("description").value),
+                            Util.asString(receiverUser.child("phoneNumber").value),
+                            Util.asString(receiverUser.child("email").value),
+                            Util.asString(receiverUser.child("occupation").value),
+                            Util.asString(receiverUser.child("linkedin").value),
+                            Util.asString(receiverUser.child("github").value),
+                            Util.asString(receiverUser.child("facebook").value),
+                            Util.asString(receiverUser.child("twitter").value)
+                        )
+                        showContactDialog(contact)
                     }
                 }
-
-                dialog = builder?.create()!!
-                dialog.show()
-
-                //until the dialog is dismissed users cannot scan a qr code
-                dialog.setOnDismissListener {
-                    mQrScanner.apply {
-                        scanMode = ScanMode.CONTINUOUS
-                    }
+            } catch (ex: Exception) {
+                requireActivity().runOnUiThread {
+                    showFailedDialog(it.toString())
                 }
+            }
+        }
+    }
+
+    private fun showContactDialog(contact: Person) {
+        val businessCardDialogView = View.inflate(context, R.layout.dialog_business_card, null)
+
+        val dialog = AlertDialog.Builder(requireActivity())
+            .setView(businessCardDialogView)
+            .create()
+
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.show()
+
+        dialog.findViewById<TextView>(R.id.network_card_new_contact)?.visibility = View.VISIBLE
+        dialog.findViewById<TextView>(R.id.network_card_name)?.text = contact.name
+        dialog.findViewById<TextView>(R.id.network_card_occupation)?.text = contact.occupation
+        dialog.findViewById<TextView>(R.id.network_card_description)?.text = contact.description
+        dialog.findViewById<TextView>(R.id.network_card_phone)?.text = contact.phone
+        dialog.findViewById<TextView>(R.id.network_card_email)?.text = contact.email
+
+        dialog.setOnDismissListener {
+            mQrScanner.apply {
+                scanMode = ScanMode.CONTINUOUS
+            }
+        }
+    }
+
+    private fun showFailedDialog(data: String) {
+        val businessCardDialogView = View.inflate(context, R.layout.dialog_business_card_failed, null)
+
+        val dialog = AlertDialog.Builder(requireActivity())
+            .setView(businessCardDialogView)
+            .create()
+
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.show()
+
+        dialog.findViewById<TextView>(R.id.network_card_failed_data)?.text = data
+
+        dialog.setOnDismissListener {
+            mQrScanner.apply {
+                scanMode = ScanMode.CONTINUOUS
             }
         }
     }
